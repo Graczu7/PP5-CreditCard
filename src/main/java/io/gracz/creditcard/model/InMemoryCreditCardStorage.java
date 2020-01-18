4aec5f3dd66d409f4eac77d34735f91e609ce92f
Grzegorz Gracz <40638361+Graczu7@users.noreply.github.com>
2020-01-18 20:50:04 +0100

    Create InMemoryCreditCardStorage.java

---
+++ src/main/java/io/gracz/creditcard/model/InMemoryCreditCardStorage.java
@@ -0,0 +1,26 @@
+package io.gracz.creditcard.model;
+
+import javax.smartcardio.Card;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+public class InMemoryCreditCardStorage implements CreditCardStorage {
+    Map<String, CreditCard> cards = new ConcurrentHashMap<String, CreditCard>();
+
+    public void add(CreditCard creditCard) {
+        cards.put(creditCard.getNumber(), creditCard);
+    }
+
+    public CreditCard load(String number) {
+        return cards.get(number);
+    }
+
+    public List<CreditCard> all() {
+        return cards.entrySet()
+                .stream()
+                .map(e -> e.getValue())
+                .collect(Collectors.toList());
+    }
+}
