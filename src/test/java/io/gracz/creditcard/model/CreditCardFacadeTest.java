3bb0d5a502fa937bf2024b0da2c7abd8bb753e7c
Grzegorz Gracz <40638361+Graczu7@users.noreply.github.com>
2020-01-18 20:54:12 +0100

    Create CreditCardFacadeTest.java

---
+++ src/test/java/io/gracz/creditcard/model/CreditCardFacadeTest.java
@@ -0,0 +1,49 @@
+package io.gracz.creditcard.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CreditCardFacadeTest {
+
+    private CreditCardStorage creditCardStorage;
+
+    @Before
+    public void setup() {
+        creditCardStorage = new InMemoryCreditCardStorage();
+    }
+
+    @Test
+    public void allowWithdrawFromCards() {
+        thereIsCreditCardIdentifiedWitNumber("1234-56789");
+
+        CreditCardFacade api = thereIsCCApi();
+        api.withdraw(new WithdrawCommand("1234-56789", BigDecimal.valueOf(200)));
+
+        CardSummary summary = api.getSummary("1234-56789");
+        Assert.assertNotNull(summary);
+        Assert.assertEquals(summary.balance, BigDecimal.valueOf(800));
+    }
+
+    @Test
+    public void itAllowGenerateReport() {
+        thereIsCreditCardIdentifiedWitNumber("1234-56789");
+        CreditCardFacade api = thereIsCCApi();
+        List<CardSummary> report = api.getCardsReport();
+        Assert.assertTrue(report.size() == 1);
+    }
+
+    private CreditCardFacade thereIsCCApi() {
+        return new CreditCardFacade(this.creditCardStorage);
+    }
+
+    private void thereIsCreditCardIdentifiedWitNumber(String number) {
+        CreditCard creditCard = new CreditCard(number);
+        creditCard.assignLimit(BigDecimal.valueOf(1000));
+
+        creditCardStorage.add(creditCard);
+    }
+}
