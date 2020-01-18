df0f7a4eaafbe5083bc459dc0b7d2b19bc05c066
Grzegorz Gracz <40638361+Graczu7@users.noreply.github.com>
2020-01-18 20:49:07 +0100

    Create FileCreditCardStorage.java

---
+++ src/main/java/io/gracz/creditcard/model/FileCreditCardStorage.java
@@ -0,0 +1,18 @@
+package io.gracz.creditcard.model;
+
+import java.util.List;
+
+public class FileCreditCardStorage implements CreditCardStorage {
+    public void add(CreditCard creditCard) {
+
+    }
+
+    public CreditCard load(String number) {
+        return null;
+    }
+
+    @Override
+    public List<CreditCard> all() {
+        return null;
+    }
+}
