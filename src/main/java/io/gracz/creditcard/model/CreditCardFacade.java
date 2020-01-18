2cb620c54e4255d52140b0c47ae65dffd2f08513
Grzegorz Gracz <40638361+Graczu7@users.noreply.github.com>
2020-01-18 20:47:43 +0100

    Create CreditCardFacade.java

---
+++ src/main/java/io/gracz/creditcard/model/CreditCardFacade.java
@@ -0,0 +1,29 @@
+package io.gracz.creditcard.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CreditCardFacade {
+    private CreditCardStorage creditCardStorage;
+
+    public CreditCardFacade(CreditCardStorage creditCardStorage) {
+        this.creditCardStorage = creditCardStorage;
+    }
+
+    public void withdraw(WithdrawCommand withdrawCommand) {
+        CreditCard creditCard = creditCardStorage.load(withdrawCommand.getNumber());
+        creditCard.withdraw(withdrawCommand.getAmount());
+        creditCardStorage.add(creditCard);
+    }
+
+    public CardSummary getSummary(String number) {
+        CreditCard creditCard = creditCardStorage.load(number);
+        return creditCard.getSummary();
+    }
+
+    public List<CardSummary> getCardsReport() {
+        return creditCardStorage.all().stream()
+                .map(CreditCard::getSummary)
+                .collect(Collectors.toList());
+    }
+}
